e59e0e2bb4f9b08c46e4033465789f419637192e
Hari <iamhari1812@gmail.com>
2026-06-12 21:52:02 +0530

    8x1mux using 4x1mux in BD IPI
    8x1 MUX using 4x1 MUX in block diagram using IP integrator. and yep it is done. had to verify the output waveform for a bit checking it if got it right. one thing i'm starting to appreciate is how much hardware design is really about verification. building the logic is only half the job. proving that it behaves exactly as intended is where most of the learning happens. every waveform feels like a conversation between what i thought i built and what i actually built.

---
+++ Practice_logs/Udemy Practice Codes/Schematic Design Entry with IP Blocks/Assignments/Assignment 20 A152.v
@@ -0,0 +1,27 @@
+// Design 8:1 Mux using 4:1 Mux
+
+module mux41(
+    input  a,b,c,d,
+    input  s0,s1,
+    output reg Y
+);
+
+always @(*) begin
+    case ({s0,s1})
+        2'b00: Y = a;
+        2'b01: Y = b;
+        2'b10: Y = c;
+        2'b11: Y = d;
+        default: Y = 1'b0;
+    endcase
+end
+
+endmodule
+
+module mux21(
+    input a,b,
+    input s2
+    output y
+);
+assign y = s2 ? b : a;
+endmodule
