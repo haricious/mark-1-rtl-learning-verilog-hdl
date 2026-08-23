aabad67ea14764097851e8d813dbd2a0dda7db3d
Hari <iamhari1812@gmail.com>
2026-08-23 17:58:36 +0530

    Auto commit 2026-08-23 17:58:36

---
+++ Finite State Machines/2_process_meth_moore.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+
+module fsm(
+    input clk, rst, din,
+    output reg dout
+);
+    
+    parameter idle = 2'b00;
+    parameter s0   = 2'b01;
+    parameter s1   = 2'b10;
+    
+    reg [1:0] state, nstate;
+    
+    // 1. Reset and state transition logic (Sequential)
+    always @(posedge clk) begin
+        if (rst == 1'b1)
+            state <= idle;
+        else
+            state <= nstate;
+    end 
+    
+    // 2. Next state logic + output logic (Combinational)
+    always @(*) begin
+        case (state)
+            idle: nstate = s0;
+            idle: dout =1'b0;
+            
+            s0: begin
+                dout =1'b0;
+                if (din == 1'b1)
+                    nstate = s1;
+                    
+                else   
+                    nstate = s0;
+            end
+            
+            s1: begin
+                dout = 1'b1;
+                if (din == 1'b1)
+                    nstate = s0;
+                else
+                    nstate = s1;
+            end
+            
+            default: begin 
+            nstate = idle; 
+            dout=idle; 
+            end
+        endcase
+    end
+    
+
+    
+    
+endmodule
