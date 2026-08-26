76c02e9965c3c0fcc342783e4a8aae7276a09866
Hari <iamhari1812@gmail.com>
2026-08-26 10:00:11 +0530

    Auto commit 2026-08-26 10:00:11

--- File IO/writing_a_file.v
+++ File IO/writing_a_file.v
@@ -5,7 +5,9 @@ integer i=0;
 
 initial begin
     file=$fopen("location","type - w, r");
-
+    for(i=0;i<=18;i=i+1) begin
+        $display(file, "format spec", "source of data") // 1. file id
+    end
 
 
 
